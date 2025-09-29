6adbf6378454f12519192c99a3a994e5fd801137
Acrylic125 <acrylic125email@gmail.com>
2025-09-29 08:29:58 +0800

    feat: general ui

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { ExploreMap } from "@/components/map";
 import { MainNavbar } from "@/components/navbar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -31,21 +32,21 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-function useBreakpointBetween(min: number, max: number) {
-  const [breakpoint, setBreakpoint] = useState<boolean>(false);
-  useEffect(() => {
-    setBreakpoint(window.innerWidth >= min && window.innerWidth <= max);
-    const handleResize = () => {
-      setBreakpoint(window.innerWidth >= min && window.innerWidth <= max);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [min, max, setBreakpoint]);
+// function useBreakpointBetween(min: number, max: number) {
+//   const [breakpoint, setBreakpoint] = useState<boolean>(false);
+//   useEffect(() => {
+//     setBreakpoint(window.innerWidth >= min && window.innerWidth <= max);
+//     const handleResize = () => {
+//       setBreakpoint(window.innerWidth >= min && window.innerWidth <= max);
+//     };
+//     window.addEventListener("resize", handleResize);
+//     return () => {
+//       window.removeEventListener("resize", handleResize);
+//     };
+//   }, [min, max, setBreakpoint]);
 
-  return breakpoint;
-}
+//   return breakpoint;
+// }
 
 function ItineraryDropdown() {
   return (
@@ -121,6 +122,33 @@ function FilterTagsDropdown() {
 export function ViewPOIReviews() {
   const auth = useAuth();
 
+  // TODO: Retrieve personal reviews + other reviews using the Reviews Router.
+  const reviews = [
+    {
+      id: 1,
+      liked: true,
+      comment: "This is a review",
+      images: ["/example.png", "/example.png"],
+      createdAt: new Date(),
+      // User needs to be retrieved from clerk.
+      // See https://clerk.com/docs/reference/backend/user/get-user-list
+      // Filter by `userId`. Note the limit is 100. We will only ever
+      // retrieve up to 50 reviews at a time so this limit will never
+      // be reached.
+      user: {
+        // Change this user id to your user id. You should see
+        // a difference.
+        id: "user_123",
+        name: "User 1",
+        profilePicture: "https://github.com/shadcn.png",
+      },
+      itinerary: {
+        id: 1,
+        name: "Itinerary 1",
+      },
+    },
+  ];
+
   return (
     <div className="flex flex-col gap-1">
       <div className="flex flex-row items-center justify-between">
@@ -128,6 +156,27 @@ export function ViewPOIReviews() {
         <Button variant="default" size="sm" disabled={!auth.isSignedIn}>
           <Plus /> Review
         </Button>
+      </div>
+      <div className="flex flex-col gap-2">
+        {reviews.map((review) => (
+          <div
+            key={review.id}
+            className={cn(
+              "flex flex-col gap-1 border border-border rounded-md p-2",
+              {
+                "bg-secondary": review.user.id === auth.userId,
+              }
+            )}
+          >
+            <div className="flex flex-row items-center gap-2">
+              <Avatar>
+                <AvatarImage src={review.user.profilePicture} />
+                <AvatarFallback>Profile</AvatarFallback>
+              </Avatar>
+              <span className="text-sm font-medium">{review.user.name}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
@@ -173,111 +222,95 @@ export function ViewPOIPanel() {
   );
 }
 
-function ExplorePageLayout({ className }: { className?: string }) {
-  // Refer to https://tailwindcss.com/docs/responsive-design
-  // for breakpoint values.
-  // <= lg implies mobile.
-  // const isMobile = useBreakpointBetween(0, 1024);
-
-  const [showItinerary, setShowItinerary] = useState<string | null>(null);
-
-  // if (isMobile) {
-  //   return <></>;
-  // }
-
-  return (
-    <div
-      className={cn("w-full h-screen-max flex flex-col md:flex-row", className)}
-    >
-      {showItinerary && (
-        <ScrollArea className="col-span-2 h-screen-max">
-          <div className="flex flex-col w-full bg-amber-400 h-screen-max"></div>
-        </ScrollArea>
-      )}
-      <div className="flex flex-col h-full flex-1">
-        <div className="flex flex-row items-center justify-between p-1 border-b border-border gap-1">
-          <div className="flex-1 flex flex-row items-center gap-2">
-            <FilterTagsDropdown />
-            <FilterDropdown />
-          </div>
-          <div className="flex-1 flex flex-row items-center gap-2">
-            <ItineraryDropdown />
-          </div>
-
-          <div className="flex-1 flex flex-row items-center gap-2 lg:hidden justify-end">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="outline">
-                  <Sparkles />{" "}
-                  <span className="hidden sm:block">Surprise Me</span>
-                  {/* <span className="hidden xs:block">Surprise Me!</span> */}
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>Filters</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuCheckboxItem>
-                  Show Visited
-                </DropdownMenuCheckboxItem>
-                <DropdownMenuCheckboxItem>
-                  Show Unvisited
-                </DropdownMenuCheckboxItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-
-          <div className="flex-1 hidden lg:flex flex-row justify-end items-center">
-            <Button variant="ghost" className="px-2.5" asChild>
-              <Link href="/surprise-me">
-                <div className="bg-background rounded-sm px-3 py-1.5 flex items-center gap-2">
-                  <Sparkles />
-                  <span className="hidden lg:block">Surprise Me!</span>
-                </div>
-              </Link>
-            </Button>
-
-            <Tabs defaultValue="account">
-              <TabsList>
-                <TabsTrigger value="account">Explore</TabsTrigger>
-                <TabsTrigger value="password">Recommend</TabsTrigger>
-              </TabsList>
-            </Tabs>
-          </div>
-        </div>
-        {/* <ExploreMap className="w-full flex-1" /> */}
-      </div>
-      <ScrollArea className="relative h-1/2 w-full lg:w-1/5 min-w-64 md:max-w-80 md:h-screen-max">
-        <ViewPOIPanel />
-        <div className="absolute flex flex-col items-center top-0 left-1/2 right-1/2 -translate-x-1/2">
-          <Tabs defaultValue="account">
-            <TabsList>
-              <TabsTrigger value="account">Itinerary</TabsTrigger>
-              <TabsTrigger value="password">Place</TabsTrigger>
-            </TabsList>
-          </Tabs>
-        </div>
-      </ScrollArea>
-    </div>
-  );
-}
-
 export default function Home() {
   return (
     <div className="w-full h-full flex flex-col items-center bg-background">
       <MainNavbar />
+      <div className="w-full h-screen-max flex flex-col md:flex-row max-w-[1920px]">
+        <div className="flex flex-col h-full flex-1">
+          <div className="flex flex-row items-center justify-between p-1 border-b border-border gap-1">
+            <div className="flex-1 flex flex-row items-center gap-2">
+              <FilterTagsDropdown />
+              <FilterDropdown />
+              <ItineraryDropdown />
+            </div>
 
-      <ExplorePageLayout className="max-w-[1920px] w-full h-screen-max" />
-      {/* <div className="bg-yellow-400 w-full h-[calc(100svh-64px)]">
-        <ExploreMap className="w-full h-full" />
-      </div> */}
-      {/* <div className="w-full flex flex-row"></div> */}
-      {/* <ScrollArea className="w-full h-[calc(100svh-64px)]">
-        <div className="w-full h-[calc(100svh-64px)] flex flex-col items-center">
-          <div className="w-full h-full max-w-screen">
-            <ExploreMap className="w-full h-full" />
+            <div className="flex-1 flex flex-row items-center gap-2 lg:hidden justify-end">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline">
+                    <Sparkles />{" "}
+                    <span className="hidden sm:block">Surprise Me</span>
+                    {/* <span className="hidden xs:block">Surprise Me!</span> */}
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>Map View</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuCheckboxItem>Explore</DropdownMenuCheckboxItem>
+                  <DropdownMenuCheckboxItem>Recommend</DropdownMenuCheckboxItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem asChild>
+                    <Link href="/surprise-me">
+                      <div className="flex items-center gap-2">
+                        <span>Take a quiz!</span>
+                      </div>
+                    </Link>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+
+            <div className="flex-1 hidden lg:flex flex-row justify-end items-center">
+              <Button variant="outline" className="px-2.5" asChild>
+                <Link href="/surprise-me">
+                  <div className="bg-background rounded-sm px-3 py-1.5 flex items-center gap-2">
+                    <Sparkles />
+                    <span className="hidden lg:block">Surprise Me!</span>
+                  </div>
+                </Link>
+              </Button>
+
+              <Tabs defaultValue="account">
+                <TabsList>
+                  <TabsTrigger value="account">Explore</TabsTrigger>
+                  <TabsTrigger value="password">Recommend</TabsTrigger>
+                </TabsList>
+              </Tabs>
+            </div>
+          </div>
+          <div className="h-full w-full p-1">
+            {/* <ExploreMap className="h-full w-full" /> */}
           </div>
         </div>
-      </ScrollArea> */}
+        <ScrollArea className="relative h-1/2 w-full lg:w-1/5 min-w-64 md:max-w-80 md:h-screen-max">
+          <ViewPOIPanel />
+          <div className="absolute flex flex-col items-center top-2 left-1/2 right-1/2 -translate-x-1/2">
+            <div className="flex flex-row bg-secondary/75 backdrop-blur-sm rounded-full border border-border p-1">
+              <Button
+                variant="secondary"
+                size="sm"
+                className="rounded-full shadow-sm text-sm py-1 px-2.5 h-fit"
+              >
+                Itinerary
+              </Button>
+              <Button
+                variant="ghost"
+                size="sm"
+                className="rounded-full text-sm py-1 px-1.5 h-fit"
+              >
+                Place
+              </Button>
+            </div>
+            {/* <Tabs defaultValue="account">
+            <TabsList>
+              <TabsTrigger value="account">Itinerary</TabsTrigger>
+              <TabsTrigger value="password">Place</TabsTrigger>
+            </TabsList>
+          </Tabs> */}
+          </div>
+        </ScrollArea>
+      </div>
     </div>
   );
 }
