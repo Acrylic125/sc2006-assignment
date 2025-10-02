439f8c303d859d779a30d8864dbc6b8e8f42e5c0
Acrylic125 <acrylic125email@gmail.com>
2025-10-02 19:17:57 +0800

    feat: map recommend

--- app/src/server/routers/map.router.ts
+++ app/src/server/routers/map.router.ts
@@ -7,7 +7,7 @@ import {
 } from "../trpc";
 import { db } from "@/db";
 import { poiImagesTable, poiTable } from "@/db/schema";
-import { eq, sql } from "drizzle-orm";
+import { and, eq, gt, lt, sql } from "drizzle-orm";
 
 export const mapRouter = createTRPCRouter({
   search: publicProcedure
@@ -30,30 +30,6 @@ export const mapRouter = createTRPCRouter({
         })
         .from(poiTable);
       return pois;
-      // TODO: Fetch from the database.
-      // const pois: {
-      //   id: number;
-      //   pos: {
-      //     latitude: number;
-      //     longitude: number;
-      //   };
-      //   popularity: number; // [0, 1], higher means more popular
-      // }[] = [];
-      // let id = 1;
-      // for (let i = -10; i < 10; i++) {
-      //   for (let j = -10; j < 10; j++) {
-      //     pois.push({
-      //       id: id++,
-      //       pos: {
-      //         latitude: 1.3521 + j * 0.01,
-      //         longitude: 103.8198 + i * 0.01,
-      //       },
-      //       // Mock the popularity
-      //       popularity: Math.abs(Math.sin(i * 0.1) * Math.cos(j * 0.1)),
-      //     });
-      //   }
-      // }
-      // return pois;
     }),
   recommend: publicOrProtectedProcedure
     .input(
@@ -69,49 +45,85 @@ export const mapRouter = createTRPCRouter({
     )
     .query(async ({ ctx, input }) => {
       const userId = ctx.auth.userId;
-      // TODO: Fetch from the database.
-      const pois: {
-        id: number;
-        pos: {
-          latitude: number;
-          longitude: number;
-        };
-        popularity: number; // [0, 1], higher means more popular
-      }[] = [];
-
       // SG is near the equator, so we will do a rough approximtion of straight
       // line dstance.
-      const longDistancePerDegree = 111.32;
-      const latDistancePerDegree = 111.32;
+      const recommendationRadius = 5 / 111.32;
 
-      const recommendationRadius = 5;
-      let id = 1;
-      for (let i = -recommendationRadius; i < recommendationRadius; i++) {
-        for (let j = -recommendationRadius; j < recommendationRadius; j++) {
-          const coords = {
-            latitude: input.fromLocation.latitude + j * 0.01,
-            longitude: input.fromLocation.longitude + i * 0.01,
-          };
-          // SG is small, dont need to complicate comoutation.
-          const distance = Math.sqrt(
-            ((coords.latitude - input.fromLocation.latitude) *
-              latDistancePerDegree) **
-              2 +
-              ((coords.longitude - input.fromLocation.longitude) *
-                longDistancePerDegree) **
-                2
-          );
-          if (distance > recommendationRadius) continue;
-
-          pois.push({
-            id: id++,
-            pos: coords,
-            // Mock the popularity
-            popularity: Math.abs(Math.sin(i * 0.1) * Math.cos(j * 0.1)),
-          });
-        }
-      }
+      const pois = await db
+        .select({
+          id: poiTable.id,
+          pos: {
+            // Parse as number
+            latitude: sql<number>`CAST(${poiTable.latitude} AS numeric)`,
+            longitude: sql<number>`CAST(${poiTable.longitude} AS numeric)`,
+          },
+        })
+        .from(poiTable)
+        .where(
+          and(
+            lt(
+              poiTable.latitude,
+              sql`CAST(${input.fromLocation.latitude + recommendationRadius} AS numeric)`
+            ),
+            gt(
+              poiTable.latitude,
+              sql`CAST(${input.fromLocation.latitude - recommendationRadius} AS numeric)`
+            ),
+            lt(
+              poiTable.longitude,
+              sql`CAST(${input.fromLocation.longitude + recommendationRadius} AS numeric)`
+            ),
+            gt(
+              poiTable.longitude,
+              sql`CAST(${input.fromLocation.longitude - recommendationRadius} AS numeric)`
+            )
+          )
+        );
       return pois;
+
+      // TODO: Fetch from the database.
+      // const pois: {
+      //   id: number;
+      //   pos: {
+      //     latitude: number;
+      //     longitude: number;
+      //   };
+      //   popularity: number; // [0, 1], higher means more popular
+      // }[] = [];
+
+      // // SG is near the equator, so we will do a rough approximtion of straight
+      // // line dstance.
+      // const longDistancePerDegree = 111.32;
+      // const latDistancePerDegree = 111.32;
+
+      // const recommendationRadius = 5;
+      // let id = 1;
+      // for (let i = -recommendationRadius; i < recommendationRadius; i++) {
+      //   for (let j = -recommendationRadius; j < recommendationRadius; j++) {
+      //     const coords = {
+      //       latitude: input.fromLocation.latitude + j * 0.01,
+      //       longitude: input.fromLocation.longitude + i * 0.01,
+      //     };
+      //     // SG is small, dont need to complicate comoutation.
+      //     const distance = Math.sqrt(
+      //       ((coords.latitude - input.fromLocation.latitude) *
+      //         latDistancePerDegree) **
+      //         2 +
+      //         ((coords.longitude - input.fromLocation.longitude) *
+      //           longDistancePerDegree) **
+      //           2
+      //     );
+      //     if (distance > recommendationRadius) continue;
+
+      //     pois.push({
+      //       id: id++,
+      //       pos: coords,
+      //       // Mock the popularity
+      //       popularity: Math.abs(Math.sin(i * 0.1) * Math.cos(j * 0.1)),
+      //     });
+      //   }
+      // }
+      // return pois;
     }),
   getPOI: publicProcedure
     .input(z.object({ id: z.number() }))
